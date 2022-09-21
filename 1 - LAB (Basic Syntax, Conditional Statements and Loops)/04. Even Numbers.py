8fe02c286d520691906d0f71cb165cadf7c325d8
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-09-21 06:21:14 +0300

    Update 04. Even Numbers.py

--- 1 - LAB (Basic Syntax, Conditional Statements and Loops)/04. Even Numbers.py
+++ 1 - LAB (Basic Syntax, Conditional Statements and Loops)/04. Even Numbers.py
@@ -10,15 +10,17 @@ else:
     print("All numbers are even.")
 
 
-# n = int(input())
+# number = int(input())
 # counter = 0
-# for count_numbers in range(1, n + 1):
+# for count_numbers in range(1, number + 1):
 #     current_number = int(input())
 #     counter += 1
 #     if current_number % 2 == 0:
-#         if counter == n:
+#         if counter == number:
 #             print("All numbers are even.")
 #             break
+#         else:
+#             continue
 #     else:
 #         print(f"{current_number} is odd!")
 #         break
