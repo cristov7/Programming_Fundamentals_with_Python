1d32068179abefd62bc0d12a7a9a4138b6adb410
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-09-22 00:03:05 +0300

    Update 07. Patterns.py

--- 1 - LAB (Basic Syntax, Conditional Statements and Loops)/07. Patterns.py
+++ 1 - LAB (Basic Syntax, Conditional Statements and Loops)/07. Patterns.py
@@ -1,7 +1,17 @@
 number = int(input())
-
 for i in range(1, number + 1, 1):
     print(i * "*")
-
 for j in range(number - 1, 0, -1):
     print(j * "*")
+
+    
+# paint = "*"
+# number = int(input())
+# count_1 = 0
+# while count_1 < number:
+#     count_1 += 1
+#     print(paint * count_1)
+# count_2 = number
+# while count_2 > 0:
+#     count_2 -= 1
+#     print(paint * count_2)
