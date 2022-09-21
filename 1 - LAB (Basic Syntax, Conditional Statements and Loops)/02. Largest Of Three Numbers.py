8ea25af361c643454bf593eb818b2aec3fb05995
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-09-21 06:48:31 +0300

    Update 02. Largest Of Three Numbers.py

--- 1 - LAB (Basic Syntax, Conditional Statements and Loops)/02. Largest Of Three Numbers.py
+++ 1 - LAB (Basic Syntax, Conditional Statements and Loops)/02. Largest Of Three Numbers.py
@@ -22,14 +22,16 @@ print(max(first_number, second_number, third_number))
 # print(max_num)
 
 
-# => Първо въвеждам число, което определя броят числа за сравнение (пр. 3):
+# => Първо въвеждам число, което определя броят числа за сравнение (пр. 3 броя числа):
 # => След това въвеждам другите числа, които искам да сравня: (пр. 1 , -20 и 7 => 7 е най-голямото число)!
 
 # import sys
 # max_number = - sys.maxsize
 # count_numbers = int(input())
-# for i in range(1, count_numbers + 1):
-#     current_num = int(input())
-#     if current_num > max_number:
-#         max_number = current_num
+# for number in range(1, count_numbers + 1):
+#     current_number = int(input())
+#     if current_number > max_number:
+#         max_number = current_number
+#     else:
+#          continue
 # print(max_number)
