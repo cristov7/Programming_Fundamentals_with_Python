4ff749f7f64eafb97d64fd785a5d68662b30f4e5
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-09-22 20:16:33 +0300

    Update 06. Next Happy Year.py

--- 2 - LAB (Data Types and Variables)/06. Next Happy Year.py
+++ 2 - LAB (Data Types and Variables)/06. Next Happy Year.py
@@ -7,3 +7,23 @@ while not happy_year_condition:
         set_year.add(str(year)[i])
     happy_year_condition = len(set_year) == len(str(year))
 print(year)
+
+
+# year = int(input())
+# new_year = year
+# happy_year = False
+# while not happy_year:
+#     new_year += 1
+#     for i in range(len(str(new_year))):
+#         for k in range(len(str(new_year))):
+#             if str(new_year)[i] != str(new_year)[k] and i != k:
+#                 happy_year = True
+#                 continue
+#             elif str(new_year)[i] == str(new_year)[k] and i != k:
+#                 happy_year = False
+#                 break
+#         if not happy_year:
+#             break
+#         else:
+#             continue
+# print(new_year)
