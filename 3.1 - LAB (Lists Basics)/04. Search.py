43064797330feb3a94cfaa10fe9ce78ffdf64ffb
cristov_7 <114032977+cristov7@users.noreply.github.com>
2022-10-03 22:23:01 +0300

    Python
    Python

--- 3.1 - LAB (Lists Basics)/04. Search.py
+++ 3.1 - LAB (Lists Basics)/04. Search.py
@@ -27,3 +27,19 @@ for words in range(1, number + 1):
         my_second_list.append(word)
 print(my_first_list)
 print(my_second_list)
+
+
+# my_list = []
+# number = int(input())
+# word = input()
+# for random_strings in range(1, number + 1):
+#     some_string = input()
+#     my_list.append(some_string)
+# print(my_list)
+# my_second_list = my_list.copy()
+# for element in my_second_list:
+#     if word in element:
+#         continue
+#     else:
+#         my_second_list.remove(element)
+# print(my_second_list)
