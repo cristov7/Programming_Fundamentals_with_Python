bc250a7d8a6e1c1af2f18709499b003a113022b4
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-05 15:41:50 +0300

    Python
    Python

--- 3.2 - EXE (Lists Basics)/06. Survival of the Biggest.py
+++ 3.2 - EXE (Lists Basics)/06. Survival of the Biggest.py
@@ -1,8 +1,19 @@
-stringList = input().split()
-numbersToRemove=int(input())
-numbersList = []
-for i in range(len(stringList)):
-    numbersList.append(int(stringList[i]))
-for j in range(numbersToRemove):
-    numbersList.remove(min(numbersList))
-print(*numbersList, sep=', ')
+numbers_as_string = input()
+numbers_as_string_in_list = numbers_as_string.split()   # .split(" ")
+count_numbers_to_remove = int(input())
+numbers_as_integers_in_list = []
+for element in numbers_as_string_in_list:
+    current_element = int(element)
+    numbers_as_integers_in_list.append(current_element)
+counter_to_remove = 0
+while True:
+    counter_to_remove += 1
+    if counter_to_remove <= count_numbers_to_remove:
+        numbers_as_integers_in_list.remove(min(numbers_as_integers_in_list))
+    else:
+        break
+update_numbers_as_string_in_list = []
+for element in numbers_as_integers_in_list:
+    current_element = str(element)
+    update_numbers_as_string_in_list.append(current_element)
+print(", ".join(update_numbers_as_string_in_list))
