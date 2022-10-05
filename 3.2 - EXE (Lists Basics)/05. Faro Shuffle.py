4d11a4c39e1903e964e5495e056a05ed95a8d573
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-05 14:11:30 +0300

    Python
    Python

--- 3.2 - EXE (Lists Basics)/05. Faro Shuffle.py
+++ 3.2 - EXE (Lists Basics)/05. Faro Shuffle.py
@@ -4,7 +4,7 @@ count_shuffles = int(input())
 for shuffle in range(1, count_shuffles + 1):
     final_deck = []
     half_deck = len(deck_of_cards_in_list) // 2   # (x // 2 == int) and (x / 2 == float)
-    left_part = deck_of_cards_in_list[0: half_deck]
+    left_part = deck_of_cards_in_list[0: half_deck]   # [0: half_deck] = [0: half_deck: 1]
     right_part = deck_of_cards_in_list[half_deck::]   # [half_deck::] = [half_deck::1]
     for card_index in range(len(left_part)):   # len(left_part) = len(right_part)
         final_deck.append(left_part[card_index])
