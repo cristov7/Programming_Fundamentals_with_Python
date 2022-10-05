366d7f3d7b61423b0663e9cfa73becc98ca62062
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-05 13:15:37 +0300

    Python
    Python

--- 3.2 - EXE (Lists Basics)/04. Number Beggars.py
+++ 3.2 - EXE (Lists Basics)/04. Number Beggars.py
@@ -5,12 +5,12 @@ takes_as_integers_in_list = []
 for takes in takes_as_strings_in_list:
     current_take = int(takes)
     takes_as_integers_in_list.append(current_take)
-final_takes_per_every_beggar = []
+final_takes_per_every_beggar_in_list = []
 starting_index_per_takes = 0
 while starting_index_per_takes < count_beggars:
     takes_per_current_beggar = 0
     for current_takes in range(starting_index_per_takes, len(takes_as_integers_in_list), count_beggars):
         takes_per_current_beggar += takes_as_integers_in_list[current_takes]
-    final_takes_per_every_beggar.append(takes_per_current_beggar)
+    final_takes_per_every_beggar_in_list.append(takes_per_current_beggar)
     starting_index_per_takes += 1
-print(final_takes_per_every_beggar)
+print(final_takes_per_every_beggar_in_list)
