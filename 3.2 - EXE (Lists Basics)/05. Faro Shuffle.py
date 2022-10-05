21d1b4ca76fb3c28e492f423740f93be968d9e8f
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-05 13:43:33 +0300

    Python
    Python

--- 3.2 - EXE (Lists Basics)/05. Faro Shuffle.py
+++ 3.2 - EXE (Lists Basics)/05. Faro Shuffle.py
@@ -1,12 +1,13 @@
-deck_of_cards = input().split()
-number_of_shuffles = int(input())
-for shuffle in range(number_of_shuffles):
+deck_of_cards_as_string = input()
+deck_of_cards_in_list = deck_of_cards_as_string.split()   # .split(" ")
+count_shuffles = int(input())
+for shuffle in range(1, count_shuffles + 1):
     final_deck = []
-    middle_of_the_deck = len(deck_of_cards) // 2
-    left_part = deck_of_cards[0: middle_of_the_deck]
-    right_part = deck_of_cards[middle_of_the_deck::]
-    for card_index in range(len(left_part)):
+    half_deck = len(deck_of_cards_in_list) // 2   # (x // 2 == int) and (x / 2 == float)
+    left_part = deck_of_cards_in_list[0: half_deck]
+    right_part = deck_of_cards_in_list[half_deck::]   # [half_deck::] = [half_deck::1]
+    for card_index in range(len(left_part)):   # len(left_part) = len(right_part)
         final_deck.append(left_part[card_index])
         final_deck.append(right_part[card_index])
-    deck_of_cards = final_deck
-print(deck_of_cards)
+    deck_of_cards_in_list = final_deck
+print(deck_of_cards_in_list)
