ff6a8c05ca6bd4cc96f0e0f54c09cfdc67d2978a
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-05 13:01:11 +0300

    Python
    Python

--- 3.2 - EXE (Lists Basics)/03. Football Cards.py
+++ 3.2 - EXE (Lists Basics)/03. Football Cards.py
@@ -27,7 +27,7 @@ else:
 
 # team_A_players = ["A-1", "A-2", "A-3", "A-4", "A-5", "A-6", "A-7", "A-8", "A-9", "A-10", "A-11"]
 # team_B_players = ["B-1", "B-2", "B-3", "B-4", "B-5", "B-6", "B-7", "B-8", "B-9", "B-10", "B-11"]
-# red_cards_for_players = input().split()
+# red_cards_for_players = input().split()   # .split(" ")
 # game_was_stopped = False
 # for player in red_cards_for_players:
 #     if player in team_A_players:
