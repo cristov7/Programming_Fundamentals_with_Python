251338fc82971a499f0d37b44f1ae1aae8d4d9d6
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-11 22:01:16 +0300

    Python
    Python

--- 1.1 - LAB (Basic Syntax, Conditional Statements and Loops)/03. Word Reverse.py
+++ 1.1 - LAB (Basic Syntax, Conditional Statements and Loops)/03. Word Reverse.py
@@ -1,8 +1,14 @@
 word = input()
 print(word[::-1])
 
+
+# word = input()
+# opposite_word = word[::-1]
+# print(opposite_word)
+
+
 # word = input()
 # reversed_word = ""
-# for index in range(len(word) - 1, -1, -1):
+# for i in range(len(word) - 1, -1, -1):
 #     reversed_word += word[i]
 # print(reversed_word)
