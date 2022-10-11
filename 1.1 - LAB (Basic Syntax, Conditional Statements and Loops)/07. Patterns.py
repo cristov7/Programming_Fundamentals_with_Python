7ef887eea5e50017e2f438b110822460da57a19f
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-11 23:02:09 +0300

    Python
    Python

--- 1.1 - LAB (Basic Syntax, Conditional Statements and Loops)/07. Patterns.py
+++ 1.1 - LAB (Basic Syntax, Conditional Statements and Loops)/07. Patterns.py
@@ -4,7 +4,21 @@ for i in range(1, number + 1, 1):
 for j in range(number - 1, 0, -1):
     print(j * "*")
 
-    
+
+# number = int(input())
+# symbol = "*"
+# ascending_orders = 0
+# for ascending_order in range(1, number + 1):
+#     ascending_orders += 1
+#     stars = symbol * ascending_orders
+#     print(symbol * ascending_orders)
+# descending_orders = ascending_orders
+# for descending_order in range(number + 1, 1, -1):
+#     descending_orders -= 1
+#     stars = symbol * descending_orders
+#     print(symbol * descending_orders)
+
+
 # paint = "*"
 # number = int(input())
 # count_1 = 0
