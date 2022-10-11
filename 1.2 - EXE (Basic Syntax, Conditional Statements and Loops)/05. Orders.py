483943fa931dc481849fb592187d1e1c51c867eb
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-12 00:36:02 +0300

    Python
    Python

--- 1.2 - EXE (Basic Syntax, Conditional Statements and Loops)/05. Orders.py
+++ 1.2 - EXE (Basic Syntax, Conditional Statements and Loops)/05. Orders.py
@@ -13,3 +13,19 @@ for order in range(count_orders):
     else:
         continue
 print(f"Total: ${total_price:.2f}")
+
+
+# total_price_per_all_orders = 0
+# count_orders = int(input())
+# for order in range(1, count_orders + 1):
+#     price_per_one_capsule = float(input())
+#     days = int(input())
+#     count_capsules_per_one_day = int(input())
+#     if 0.01 <= price_per_one_capsule <= 100 and 1 <= days <= 31 and 1 <= count_capsules_per_one_day <= 2000:
+#         total_price_per_order = price_per_one_capsule * count_capsules_per_one_day * days
+#         print(f"The price for the coffee is: ${total_price_per_order:.2f}")
+#         total_price_per_all_orders += total_price_per_order
+#     else:
+#         continue
+# else:
+#     print(f"Total: ${total_price_per_all_orders:.2f}")
