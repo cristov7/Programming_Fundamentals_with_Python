9c6e9c1b5b0fef4bc231c02b8ab834f89fa8958d
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-11 23:07:48 +0300

    Python
    Python

--- 1.2 - EXE (Basic Syntax, Conditional Statements and Loops)/01. Jenny's Secret Message.py
+++ 1.2 - EXE (Basic Syntax, Conditional Statements and Loops)/01. Jenny's Secret Message.py
@@ -3,3 +3,10 @@ if name != "Johnny":
     print(f"Hello, {name}!")
 else:
     print("Hello, my love!")
+
+
+# name = input()
+# if name == "Johnny":
+#     print(f"Hello, my love!")
+# else:
+#     print(f"Hello, {name}!")
