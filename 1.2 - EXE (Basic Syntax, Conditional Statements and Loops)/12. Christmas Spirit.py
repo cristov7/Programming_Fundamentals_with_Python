c58dc68536db7a813538fb37f93174fa9ae0ded0
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-12 02:47:37 +0300

    Python
    Python

--- 1.2 - EXE (Basic Syntax, Conditional Statements and Loops)/12. Christmas Spirit.py
+++ 1.2 - EXE (Basic Syntax, Conditional Statements and Loops)/12. Christmas Spirit.py
@@ -41,3 +41,41 @@ if days_to_christmas % 10 == 0:
     total_spirit += christmas_spirit
 print(f"Total cost: {total_cost}")
 print(f"Total spirit: {total_spirit}")
+
+
+# quantity = int(input())
+# days = int(input())
+# price_per_one_ornament_set = 2
+# points_per_one_ornament_set = 5
+# price_per_one_tree_skirt = 5
+# points_per_one_tree_skirt = 3
+# price_per_one_tree_garland = 3
+# points_per_one_tree_garland = 10
+# price_per_one_tree_lights = 15
+# points_per_one_tree_lights = 17
+# total_expenses = 0
+# total_spirit = 0
+# for day in range(1, days + 1):
+#     if day % 11 == 0:
+#         quantity += 2
+#     if day % 2 == 0:
+#         total_expenses += (price_per_one_ornament_set * quantity)
+#         total_spirit += points_per_one_ornament_set
+#     if day % 3 == 0:
+#         total_expenses += (price_per_one_tree_skirt * quantity) + (price_per_one_tree_garland * quantity)
+#         total_spirit += points_per_one_tree_skirt + points_per_one_tree_garland
+#     if day % 5 == 0:
+#         total_expenses += (price_per_one_tree_lights * quantity)
+#         total_spirit += points_per_one_tree_lights
+#     if day % 3 == 0 and day % 5 == 0:
+#         total_spirit += 30
+#     if day % 10 == 0:
+#         total_spirit -= 20
+#         total_expenses += price_per_one_tree_skirt + price_per_one_tree_garland + price_per_one_tree_lights
+# if days % 10 == 0:
+#     total_spirit -= 30
+#     print(f"Total cost: {total_expenses}")
+#     print(f"Total spirit: {total_spirit}")
+# else:
+#     print(f"Total cost: {total_expenses}")
+#     print(f"Total spirit: {total_spirit}")
