2b0c6f9d40507b574b895611cac56d5cea20ffb8
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-12 13:34:00 +0300

    Python
    Python

--- 2.2 - EXE (Data Types and Variables)/07. Water Overflow.py
+++ 2.2 - EXE (Data Types and Variables)/07. Water Overflow.py
@@ -1,13 +1,27 @@
-max_capacity = 255
-water_in_reservoir = 0
-count_times = int(input())
-for current_time in range(1, count_times + 1):
-    liters_water = int(input())
-    if liters_water + water_in_reservoir <= max_capacity:
-        water_in_reservoir += liters_water
+reservoir_max_liters = 255
+reservoir_in_liters = 0
+number = int(input())
+for water_liters in range(1, number + 1):
+    liters = int(input())
+    if reservoir_max_liters >= liters:
+        reservoir_max_liters -= liters
+        reservoir_in_liters += liters
     else:
         print("Insufficient capacity!")
-print(water_in_reservoir)
+else:
+    print(reservoir_in_liters)
+
+
+# max_capacity = 255
+# water_in_reservoir = 0
+# count_times = int(input())
+# for current_time in range(1, count_times + 1):
+#     liters_water = int(input())
+#     if liters_water + water_in_reservoir <= max_capacity:
+#         water_in_reservoir += liters_water
+#     else:
+#         print("Insufficient capacity!")
+# print(water_in_reservoir)
 
 
 # count_times = int(input())
