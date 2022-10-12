07fb27eaf01bcc0c25b7f662541c93cad09c5b9b
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-12 06:11:16 +0300

    Python
    Python

--- 2.2 - EXE (Data Types and Variables)/03. Elevator.py
+++ 2.2 - EXE (Data Types and Variables)/03. Elevator.py
@@ -1,8 +1,21 @@
-from math import ceil
 count_people = int(input())
-elevator_max_capacity = int(input())
-courses = ceil(count_people / elevator_max_capacity)
-print(courses)
+max_capacity = int(input())
+courses = 0
+while True:
+    if count_people - max_capacity <= 0:
+        courses += 1
+        print(courses)
+        break
+    else:
+        courses += 1
+        count_people -= max_capacity
+
+
+# from math import ceil
+# count_people = int(input())
+# elevator_max_capacity = int(input())
+# courses = ceil(count_people / elevator_max_capacity)
+# print(courses)
 
 
 # from math import ceil
