4346ad5f68a31c385a203dcb87ff0f4faca3778a
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-12 20:12:13 +0300

    Python
    Python

--- 3.1 - LAB (Lists Basics)/02. Courses.py
+++ 3.1 - LAB (Lists Basics)/02. Courses.py
@@ -1,6 +1,14 @@
-number = int(input())
-list_of_courses = []
-for course in range(1, number + 1):
+# number = int(input())
+# list_of_courses = []
+# for course in range(1, number + 1):
+#     name_of_course = input()
+#     list_of_courses.append(name_of_course)
+# print(list_of_courses)
+
+
+list_of_courses = list()
+count_number = int(input())
+for course in range(1, count_number + 1):
     name_of_course = input()
     list_of_courses.append(name_of_course)
 print(list_of_courses)
