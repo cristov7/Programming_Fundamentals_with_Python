664fcf6c843b29afafbdd819f34c87d327708421
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-12 06:33:51 +0300

    Python
    Python

--- 2.2 - EXE (Data Types and Variables)/05. Print Part of the ASCII Table.py
+++ 2.2 - EXE (Data Types and Variables)/05. Print Part of the ASCII Table.py
@@ -2,3 +2,12 @@ start_index = int(input())
 finish_index = int(input())
 for index in range(start_index, finish_index + 1):
     print(chr(index), end=" ")
+
+    
+# start_number = int(input())
+# finish_number = int(input())
+# ascii_table_list = []
+# for ascii_numbers in range(start_number, finish_number + 1):
+#     current_number = chr(ascii_numbers)
+#     ascii_table_list.append(current_number)
+# print(*ascii_table_list)   # (" ".join(ascii_table_list))
