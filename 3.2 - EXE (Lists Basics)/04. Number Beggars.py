b7c57e0fcd49c1fe384160516546bacd39185c3f
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-13 21:36:44 +0300

    Solutions
    Solutions

--- 3.2 - EXE (Lists Basics)/04. Number Beggars.py
+++ 3.2 - EXE (Lists Basics)/04. Number Beggars.py
@@ -1,16 +1,38 @@
-takes_as_strings = input()
+numbers_as_string = input()
 count_beggars = int(input())
-takes_as_strings_in_list = takes_as_strings.split(", ")
-takes_as_integers_in_list = []
-for takes in takes_as_strings_in_list:
-    current_take = int(takes)
-    takes_as_integers_in_list.append(current_take)
-final_takes_per_every_beggar_in_list = []
-starting_index_per_takes = 0
-while starting_index_per_takes < count_beggars:
-    takes_per_current_beggar = 0
-    for current_takes in range(starting_index_per_takes, len(takes_as_integers_in_list), count_beggars):
-        takes_per_current_beggar += takes_as_integers_in_list[current_takes]
-    final_takes_per_every_beggar_in_list.append(takes_per_current_beggar)
-    starting_index_per_takes += 1
-print(final_takes_per_every_beggar_in_list)
+numbers_as_string_in_list = numbers_as_string.split(", ")
+numbers_as_integer_in_list = list()
+for element in numbers_as_string_in_list:
+    current_element = int(element)
+    numbers_as_integer_in_list.append(current_element)
+sum_per_beggar_in_list = []
+for beggar in range(count_beggars):
+    index = beggar
+    sum_per_beggar = 0
+    for takes in range(len(numbers_as_integer_in_list)):
+        if index < len(numbers_as_string_in_list):
+            current_take = numbers_as_integer_in_list[index]
+            sum_per_beggar += current_take
+            index += count_beggars
+        else:
+            break
+    sum_per_beggar_in_list.append(sum_per_beggar)
+print(sum_per_beggar_in_list)
+
+
+# takes_as_strings = input()
+# count_beggars = int(input())
+# takes_as_strings_in_list = takes_as_strings.split(", ")
+# takes_as_integers_in_list = []
+# for takes in takes_as_strings_in_list:
+#     current_take = int(takes)
+#     takes_as_integers_in_list.append(current_take)
+# final_takes_per_every_beggar_in_list = []
+# starting_index_per_takes = 0
+# while starting_index_per_takes < count_beggars:
+#     takes_per_current_beggar = 0
+#     for current_takes in range(starting_index_per_takes, len(takes_as_integers_in_list), count_beggars):
+#         takes_per_current_beggar += takes_as_integers_in_list[current_takes]
+#     final_takes_per_every_beggar_in_list.append(takes_per_current_beggar)
+#     starting_index_per_takes += 1
+# print(final_takes_per_every_beggar_in_list)
