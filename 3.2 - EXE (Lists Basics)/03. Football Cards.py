555ead64948275b736a762313636836d857aee7e
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-13 20:39:57 +0300

    Solutions
    Solutions

--- 3.2 - EXE (Lists Basics)/03. Football Cards.py
+++ 3.2 - EXE (Lists Basics)/03. Football Cards.py
@@ -1,33 +1,35 @@
-team_A_players_list = ["A-1", "A-2", "A-3", "A-4", "A-5", "A-6", "A-7", "A-8", "A-9", "A-10", "A-11"]
-team_B_players_list = ["B-1", "B-2", "B-3", "B-4", "B-5", "B-6", "B-7", "B-8", "B-9", "B-10", "B-11"]
-red_cards_in_string = input()
-red_cards_list = red_cards_in_string.split(" ")   # red_cards_list = red_cards_in_string.split()
-for player in red_cards_list:
-    if player in team_A_players_list:
-        team_A_players_list.remove(player)
-        if len(team_A_players_list) < 7:
-            print(f"Team A - {len(team_A_players_list)}; Team B - {len(team_B_players_list)}")
-            print("Game was terminated")
+team_A_list = ['A-1', 'A-2', 'A-3', 'A-4', 'A-5', 'A-6', 'A-7', 'A-8', 'A-9', 'A-10', 'A-11']
+team_B_list = ['B-1', 'B-2', 'B-3', 'B-4', 'B-5', 'B-6', 'B-7', 'B-8', 'B-9', 'B-10', 'B-11']
+game_was_terminated = False
+input_red_cards_as_string = input()
+red_cards_in_list = input_red_cards_as_string.split(" ")   # .split()
+for red_card in red_cards_in_list:
+    if red_card in team_A_list:
+        team_A_list.remove(red_card)
+        if len(team_A_list) < 7:
+            game_was_terminated = True
             break
         else:
             continue
-    elif player in team_B_players_list:
-        team_B_players_list.remove(player)
-        if len(team_B_players_list) < 7:
-            print(f"Team A - {len(team_A_players_list)}; Team B - {len(team_B_players_list)}")
-            print("Game was terminated")
+    elif red_card in team_B_list:
+        team_B_list.remove(red_card)
+        if len(team_B_list) < 7:
+            game_was_terminated = True
             break
         else:
             continue
     else:
         continue
+if game_was_terminated:   # if game_was_terminated == True:
+    print(f"Team A - {len(team_A_list)}; Team B - {len(team_B_list)}")
+    print("Game was terminated")
 else:
-    print(f"Team A - {len(team_A_players_list)}; Team B - {len(team_B_players_list)}")
+    print(f"Team A - {len(team_A_list)}; Team B - {len(team_B_list)}")
 
 
 # team_A_players = ["A-1", "A-2", "A-3", "A-4", "A-5", "A-6", "A-7", "A-8", "A-9", "A-10", "A-11"]
 # team_B_players = ["B-1", "B-2", "B-3", "B-4", "B-5", "B-6", "B-7", "B-8", "B-9", "B-10", "B-11"]
-# red_cards_for_players = input().split()   # .split(" ")
+# red_cards_for_players = input().split()
 # game_was_stopped = False
 # for player in red_cards_for_players:
 #     if player in team_A_players:
@@ -42,5 +44,33 @@ else:
 #     print("Game was terminated")
 
 
+# team_A_players_list = ["A-1", "A-2", "A-3", "A-4", "A-5", "A-6", "A-7", "A-8", "A-9", "A-10", "A-11"]
+# team_B_players_list = ["B-1", "B-2", "B-3", "B-4", "B-5", "B-6", "B-7", "B-8", "B-9", "B-10", "B-11"]
+# red_cards_in_string = input()
+# red_cards_list = red_cards_in_string.split(" ")   # red_cards_list = red_cards_in_string.split()
+# for player in red_cards_list:
+#     if player in team_A_players_list:
+#         team_A_players_list.remove(player)
+#         if len(team_A_players_list) < 7:
+#             print(f"Team A - {len(team_A_players_list)}; Team B - {len(team_B_players_list)}")
+#             print("Game was terminated")
+#             break
+#         else:
+#             continue
+#     elif player in team_B_players_list:
+#         team_B_players_list.remove(player)
+#         if len(team_B_players_list) < 7:
+#             print(f"Team A - {len(team_A_players_list)}; Team B - {len(team_B_players_list)}")
+#             print("Game was terminated")
+#             break
+#         else:
+#             continue
+#     else:
+#         continue
+# else:
+#     print(f"Team A - {len(team_A_players_list)}; Team B - {len(team_B_players_list)}")
+
+
 # team_A_players = ["A-" + str(s) for s in range(1, 12)]
 # team_B_players = ["B-" + str(s) for s in range(1, 12)]
+
