724a4810aad290932bba3f4029012acc0ee8e358
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-13 14:20:17 +0300

    Python
    Python

--- 3.2 - EXE (Lists Basics)/01. Invert Values.py
+++ 3.2 - EXE (Lists Basics)/01. Invert Values.py
@@ -1,10 +1,19 @@
-numbers_in_string = input()
-my_numbers_list = numbers_in_string.split(" ")   # my_numbers_list = numbers_in_string.split()
-my_opposite_list = []
-for element in my_numbers_list:
-    current_element = -int(element)
-    my_opposite_list.append(current_element)
-print(my_opposite_list)
+numbers_as_string = input()
+numbers_as_string_in_list = numbers_as_string.split(" ")
+invert_values_list = []
+for element in numbers_as_string_in_list:
+    current_element = int(element) * (-1)
+    invert_values_list.append(current_element)
+print(invert_values_list)
+
+
+# numbers_in_string = input()
+# my_numbers_list = numbers_in_string.split(" ")   # my_numbers_list = numbers_in_string.split()
+# my_opposite_list = []
+# for element in my_numbers_list:
+#     current_element = -int(element)
+#     my_opposite_list.append(current_element)
+# print(my_opposite_list)
 
 
 # string_of_numbers = input()
