aad02fed9cd673085e5c7a4473326fc4602aa27d
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-13 16:48:19 +0300

    Python
    Python

--- 1.3 - MORE EXE (Basic Syntax, Conditional Statements and Loops)/03. Wolf In Sheep's Clothing.py
+++ 1.3 - MORE EXE (Basic Syntax, Conditional Statements and Loops)/03. Wolf In Sheep's Clothing.py
@@ -1,11 +1,26 @@
 animals_in_list = input().split(", ")
-if animals_in_list[-1] == "wolf":
+animals_in_list.reverse()
+if animals_in_list[0] == "wolf":
     print("Please go away and stop eating my sheep")
 else:
-    while True:
-        if animals_in_list[0] == "sheep":
-            animals_in_list.remove("sheep")
-        else:
-            sheep_in_danger = len(animals_in_list) - 1
+    sheep_in_danger = 0
+    for index, value in enumerate(animals_in_list):
+        if value == "wolf":
             print(f"Oi! Sheep number {sheep_in_danger}! You are about to be eaten by a wolf!")
             break
+        else:
+            sheep_in_danger += 1
+            continue
+
+
+# animals_in_list = input().split(", ")
+# if animals_in_list[-1] == "wolf":
+#     print("Please go away and stop eating my sheep")
+# else:
+#     while True:
+#         if animals_in_list[0] == "sheep":
+#             animals_in_list.remove("sheep")
+#         else:
+#             sheep_in_danger = len(animals_in_list) - 1
+#             print(f"Oi! Sheep number {sheep_in_danger}! You are about to be eaten by a wolf!")
+#             break
