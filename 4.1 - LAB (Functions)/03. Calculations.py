6a10bbdbfbf64b6c5ac256e0a6157d46855fdb44
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-15 04:27:38 +0300

    Solutions
    Solutions

--- 4.1 - LAB (Functions)/03. Calculations.py
+++ 4.1 - LAB (Functions)/03. Calculations.py
@@ -1,3 +1,22 @@
+operator = input()
+first_number = int(input())
+second_number = int(input())
+
+
+def calculation(choice, number_1, number_2):
+    if choice == "multiply":
+        return number_1 * number_2
+    elif choice == "divide":
+        return number_1 // number_2
+    elif choice == "add":
+        return number_1 + number_2
+    elif choice == "subtract":
+        return number_1 - number_2
+
+
+print(calculation(operator, first_number, second_number))
+
+
 # operator = input()
 # first_number = int(input())
 # second_number = int(input())
@@ -17,20 +36,20 @@
 # print(result(operator, first_number, second_number))
 
 
-def calculation(first_number, second_number, operator):
-    result = None
-    if operator == "multiply":
-        result = first_number * second_number
-    elif operator == "divide":
-        result = int(first_number / second_number)
-    elif operator == "add":
-        result = first_number + second_number
-    elif operator == "subtract":
-        result = first_number - second_number
-    print(result)
-
-
-input_operator = input()
-input_first_number = int(input())
-input_second_number = int(input())
-calculation(input_first_number, input_second_number, input_operator)
+# def calculation(first_number, second_number, operator):
+#     result = None
+#     if operator == "multiply":
+#         result = first_number * second_number
+#     elif operator == "divide":
+#         result = int(first_number / second_number)
+#     elif operator == "add":
+#         result = first_number + second_number
+#     elif operator == "subtract":
+#         result = first_number - second_number
+#     print(result)
+# 
+# 
+# input_operator = input()
+# input_first_number = int(input())
+# input_second_number = int(input())
+# calculation(input_first_number, input_second_number, input_operator)
