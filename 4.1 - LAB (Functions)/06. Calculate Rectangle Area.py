9c5ec6445ef565778d56e13f5321f276c9f1cd60
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-15 05:17:21 +0300

    Solutions
    Solutions

--- 4.1 - LAB (Functions)/06. Calculate Rectangle Area.py
+++ 4.1 - LAB (Functions)/06. Calculate Rectangle Area.py
@@ -1,12 +1,30 @@
-def calculation(first_side, second_side):
-    calculated_area = first_side * second_side
-    return calculated_area
+width = int(input())
+height = int(input())
+area = lambda a, b: a * b
+print(area(width, height))
 
 
-side_a = int(input())
-side_b = int(input())
-area = calculation(side_a, side_b)
-print(area)
+# width = int(input())
+# height = int(input())
+#
+#
+# def rectangle_area(a, b):
+#     area = a * b
+#     return area
+#
+#
+# print(rectangle_area(width, height))
+
+
+# def calculation(first_side, second_side):
+#     calculated_area = first_side * second_side
+#     return calculated_area
+# 
+# 
+# side_a = int(input())
+# side_b = int(input())
+# area = calculation(side_a, side_b)
+# print(area)
 
 
 # def calculating(side_a, side_b):
