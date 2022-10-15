e51974c920fa459f0f5816f95c7c619097a5d409
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-15 04:43:09 +0300

    Solutions
    Solutions

--- 4.1 - LAB (Functions)/04. Repeat String.py
+++ 4.1 - LAB (Functions)/04. Repeat String.py
@@ -1,3 +1,21 @@
+input_string = input()
+counter = int(input())
+
+
+def repeat_string_function(variable_string, count_repeat):
+    return variable_string * count_repeat
+
+
+print(repeat_string_function(input_string, counter))
+
+
+# input_string = input()
+# counter = int(input())
+# repeat_string = lambda a, b: a * b
+# result = repeat_string(input_string, counter)
+# print(result)
+
+
 # string_text = input()
 # counter = int(input())
 #
@@ -9,7 +27,7 @@
 # print(repeat_string_text(string_text, counter))
 
 
-string_text = input()
-counter = int(input())
-repeat_string_text = lambda string_text, counter: string_text * counter
-print(repeat_string_text(string_text, counter))
+# string_text = input()
+# counter = int(input())
+# repeat_string_text = lambda string_text, counter: string_text * counter
+# print(repeat_string_text(string_text, counter))
