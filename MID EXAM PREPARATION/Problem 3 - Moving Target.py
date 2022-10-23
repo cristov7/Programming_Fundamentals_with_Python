38d4b6daa4c67c0904f6ae807e17b2b352c10157
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-23 19:44:38 +0300

    MID EXAM PREPARATION
    MID EXAM PREPARATION

--- MID EXAM PREPARATION/Problem 3 - Moving Target.py
+++ MID EXAM PREPARATION/Problem 3 - Moving Target.py
@@ -45,3 +45,51 @@ while True:
                 continue
 numbers_list_as_strings = list(map(str, numbers_list))
 print("|".join(numbers_list_as_strings))
+
+
+# def shoot_func(index, power, targets):
+#     if 0 <= index < len(targets):
+#         if targets[index] - power <= 0:
+#             targets.pop(index)
+#         else:
+#             targets[index] -= power
+#     return targets
+# 
+# 
+# def add_func(index, value, targets):
+#     if 0 <= index < len(targets):
+#         targets.insert(index, value)
+#     else:
+#         print("Invalid placement!")
+#     return targets
+# 
+# 
+# def strike_func(index, radius, targets):
+#     validator_index = index - radius >= 0 and index + radius < len(targets)
+#     if validator_index:
+#         start_target_index = index - radius
+#         final_target_index = index + radius
+#         targets = [targets[i] for i in range(len(targets)) if i < start_target_index or i > final_target_index]
+#     else:
+#         print("Strike missed!")
+#     return targets
+# 
+# 
+# def moving_target(targets):
+#     while True:
+#         command = input()
+#         if command == "End":
+#             break
+#         current_command, first_element, second_element = command.split()
+#         if current_command == "Shoot":
+#             targets = shoot_func(int(first_element), int(second_element), targets)
+#         elif current_command == "Add":
+#             targets = add_func(int(first_element), int(second_element), targets)
+#         elif current_command == "Strike":
+#             targets = strike_func(int(first_element), int(second_element), targets)
+#     result = "|".join([str(num) for num in targets])
+#     print(result)
+# 
+# 
+# data = list(map(int, input().split()))
+# moving_target(data)
