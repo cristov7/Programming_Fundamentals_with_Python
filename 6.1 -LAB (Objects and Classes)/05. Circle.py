6605fc92019042f6fd6c4cf2fedab9094562f97e
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-10-29 01:53:37 +0300

    Solutions
    Solutions

--- 6.1 -LAB (Objects and Classes)/05. Circle.py
+++ 6.1 -LAB (Objects and Classes)/05. Circle.py
@@ -6,13 +6,13 @@ class Circle:
         self.radius = diameter / 2
 
     def calculate_circumference(self):
-        return Circle.__pi * self.diameter
+        return 2 * Circle.__pi * self.radius
 
     def calculate_area(self):
-        return Circle.__pi * self.radius * self.radius
+        return Circle.__pi * self.radius ** 2
 
     def calculate_area_of_sector(self, angle):
-        return (angle / 360) * Circle.__pi * self.radius * self.radius
+        return Circle.__pi * self.radius * self.radius * (angle / 360)
 
 
 circle = Circle(10)
@@ -20,6 +20,30 @@ angle = 5
 print(f"{circle.calculate_circumference():.2f}")
 print(f"{circle.calculate_area():.2f}")
 print(f"{circle.calculate_area_of_sector(angle):.2f}")
+
+
+# class Circle:
+#     __pi = 3.14
+#
+#     def __init__(self, diameter):
+#         self.diameter = diameter
+#         self.radius = diameter / 2
+#
+#     def calculate_circumference(self):
+#         return Circle.__pi * self.diameter
+#
+#     def calculate_area(self):
+#         return Circle.__pi * self.radius * self.radius
+#
+#     def calculate_area_of_sector(self, angle):
+#         return (angle / 360) * Circle.__pi * self.radius * self.radius
+#
+#
+# circle = Circle(10)
+# angle = 5
+# print(f"{circle.calculate_circumference():.2f}")
+# print(f"{circle.calculate_area():.2f}")
+# print(f"{circle.calculate_area_of_sector(angle):.2f}")
 
 
 # class Circle:
