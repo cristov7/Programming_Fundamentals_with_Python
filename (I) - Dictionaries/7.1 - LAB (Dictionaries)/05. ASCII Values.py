e7a17c52bcc1aeb6373b5a20a7bce3b50e731b56
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-11-08 09:03:12 +0200

    Python
    Python

--- (I) - Dictionaries/7.1 - LAB (Dictionaries)/05. ASCII Values.py
+++ (I) - Dictionaries/7.1 - LAB (Dictionaries)/05. ASCII Values.py
@@ -1,3 +1,11 @@
-chars_list = input().split(", ")
-dictionary = {element: ord(element) for element in chars_list}
-print(dictionary)
+list_of_characters = input().split(", ")
+ascii_info = {}
+for character in list_of_characters:
+    ascii_value = ord(character)
+    ascii_info[character] = int(ascii_value)
+print(ascii_info)
+
+
+# chars_list = input().split(", ")
+# dictionary = {element: ord(element) for element in chars_list}
+# print(dictionary)
