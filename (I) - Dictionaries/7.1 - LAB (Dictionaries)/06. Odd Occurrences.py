9a3fb8449272c9805c2b6534e332bd818485f1e8
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-11-08 09:25:36 +0200

    Python
    Python

--- (I) - Dictionaries/7.1 - LAB (Dictionaries)/06. Odd Occurrences.py
+++ (I) - Dictionaries/7.1 - LAB (Dictionaries)/06. Odd Occurrences.py
@@ -1,13 +1,28 @@
-words_list = [element.lower() for element in input().split()]
-dictionary = {}
-for word in words_list:
-    count_word = words_list.count(word)
-    if count_word % 2:
-        dictionary[word] = f" {count_word} times"
+words = [word.lower() for word in input().split()]
+word_and_counter = {}
+for word in words:
+    if word not in word_and_counter.keys():
+        count_word = words.count(word)
+        word_and_counter[word] = count_word
     else:
         continue
-for key in dictionary:   # for key in dictionary.keys():
-    print(key, end=" ")
+for word, number in word_and_counter.items():
+    if number % 2:
+        print(f"{word}", end=" ")
+    else:
+        continue
+
+
+# words_list = [element.lower() for element in input().split()]
+# dictionary = {}
+# for word in words_list:
+#     count_word = words_list.count(word)
+#     if count_word % 2:
+#         dictionary[word] = f" {count_word} times"
+#     else:
+#         continue
+# for key in dictionary:   # for key in dictionary.keys():
+#     print(key, end=" ")
 
 
 # words = input().split()
