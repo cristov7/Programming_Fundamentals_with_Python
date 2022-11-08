af1bf4dd0550125a75e03bfe6415747818072e26
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-11-08 09:34:13 +0200

    Python
    Python

--- (I) - Dictionaries/7.1 - LAB (Dictionaries)/07. Word Synonyms.py
+++ (I) - Dictionaries/7.1 - LAB (Dictionaries)/07. Word Synonyms.py
@@ -1,14 +1,27 @@
+word_synonyms = {}   # key = word, value = list of synonyms
 count_words = int(input())
-dictionary = {}
 for word_and_synonym in range(1, count_words + 1):
     word = input()
     synonym = input()
-    if word not in dictionary:   # if word not in dictionary:
-        dictionary[word] = [synonym]
+    if word not in word_synonyms.keys():
+        word_synonyms[word] = [synonym]
     else:
-        dictionary[word].append(synonym)
-for current_word, current_synonym in dictionary.items():
-    print(f"{current_word} - {', '.join(current_synonym)}")
+        word_synonyms[word].append(synonym)
+for word, synonyms in word_synonyms.items():
+    print(f"{word} - {', '.join(synonyms)}")
+
+
+# count_words = int(input())
+# dictionary = {}
+# for word_and_synonym in range(1, count_words + 1):
+#     word = input()
+#     synonym = input()
+#     if word not in dictionary:   # if word not in dictionary:
+#         dictionary[word] = [synonym]
+#     else:
+#         dictionary[word].append(synonym)
+# for current_word, current_synonym in dictionary.items():
+#     print(f"{current_word} - {', '.join(current_synonym)}")
 
 
 # number = int(input())
