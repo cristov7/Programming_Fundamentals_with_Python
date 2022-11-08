ba5782fd069a04ab45628b53a2ea63e1b621f7b7
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-11-09 01:54:35 +0200

    Python
    Python

--- (I) - Dictionaries/7.2 - EXE (Dictionaries)/02. A Miner Task.py
+++ (I) - Dictionaries/7.2 - EXE (Dictionaries)/02. A Miner Task.py
@@ -1,17 +1,33 @@
-miner_task = {}
+resource_quantity = {}
 while True:
     command = input()
     if command == "stop":
         break
     else:
-        current_resource = command
-        current_quantity = int(input())
-        if current_resource not in miner_task:   # if current_resource not in miner_task.keys():
-            miner_task[current_resource] = current_quantity
+        resource = command
+        quantity = int(input())
+        if resource not in resource_quantity:
+            resource_quantity[resource] = quantity
         else:
-            miner_task[current_resource] += current_quantity
-for resource, quantity in miner_task.items():
+            resource_quantity[resource] += quantity
+for resource, quantity in resource_quantity.items():
     print(f"{resource} -> {quantity}")
+
+
+# miner_task = {}
+# while True:
+#     command = input()
+#     if command == "stop":
+#         break
+#     else:
+#         current_resource = command
+#         current_quantity = int(input())
+#         if current_resource not in miner_task:   # if current_resource not in miner_task.keys():
+#             miner_task[current_resource] = current_quantity
+#         else:
+#             miner_task[current_resource] += current_quantity
+# for resource, quantity in miner_task.items():
+#     print(f"{resource} -> {quantity}")
 
 
 # resources = {}
