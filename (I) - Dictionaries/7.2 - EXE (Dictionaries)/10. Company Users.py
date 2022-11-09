f32c294cfd450280398c74141f00e28d01590527
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-11-09 07:02:20 +0200

    Python
    Python

--- (I) - Dictionaries/7.2 - EXE (Dictionaries)/10. Company Users.py
+++ (I) - Dictionaries/7.2 - EXE (Dictionaries)/10. Company Users.py
@@ -2,24 +2,46 @@ company_users = {}
 while True:
     command = input()
     if command == "End":
+        for company, employee_ids in company_users.items():
+            print(f"{company}")
+            for employee_id in employee_ids:
+                print(f"-- {employee_id}")
         break
     else:
-        # company_name, employee_id = command.split(" -> ")
-        info_list = command.split(" -> ")
-        company_name = info_list[0]
-        employee_id = info_list[1]
-        if company_name not in company_users.keys():   # if company_name not in company_users:
+        command_list = command.split(" -> ")
+        company_name = command_list[0]
+        employee_id = command_list[1]
+        if company_name not in company_users:
             company_users[company_name] = [employee_id]
         else:
-            if employee_id in company_users[company_name]:
-                continue
-            else:
+            if employee_id not in company_users[company_name]:
                 company_users[company_name].append(employee_id)
-for company, users in company_users.items():
-    print(f"{company}")
-    count_users = len(users)
-    for user in range(count_users):
-        print(f"-- {users[user]}")
+            else:
+                continue
+
+
+# company_users = {}
+# while True:
+#     command = input()
+#     if command == "End":
+#         break
+#     else:
+#         # company_name, employee_id = command.split(" -> ")
+#         info_list = command.split(" -> ")
+#         company_name = info_list[0]
+#         employee_id = info_list[1]
+#         if company_name not in company_users.keys():   # if company_name not in company_users:
+#             company_users[company_name] = [employee_id]
+#         else:
+#             if employee_id in company_users[company_name]:
+#                 continue
+#             else:
+#                 company_users[company_name].append(employee_id)
+# for company, users in company_users.items():
+#     print(f"{company}")
+#     count_users = len(users)
+#     for user in range(count_users):
+#         print(f"-- {users[user]}")
 
 
 # companies_dict = {}
