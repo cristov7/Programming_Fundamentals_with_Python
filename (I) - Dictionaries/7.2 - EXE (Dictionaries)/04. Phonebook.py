8663c4b539b9cfda708ec09d07fb9462b12d9897
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-11-09 03:43:19 +0200

    Python
    Python

--- (I) - Dictionaries/7.2 - EXE (Dictionaries)/04. Phonebook.py
+++ (I) - Dictionaries/7.2 - EXE (Dictionaries)/04. Phonebook.py
@@ -3,24 +3,47 @@ while True:
     info = input()
     if info.isdigit():
         number = int(info)
-        for information in range(1, number + 1):
-            name = input()
-            if name in phonebook:   # if name in phonebook.keys():
-                number = phonebook[name]
-                print(f"{name} -> {number}")
+        for people in range(1, number + 1):
+            contact = input()
+            if contact in phonebook:
+                phone_number = phonebook[contact]
+                print(f"{contact} -> {phone_number}")
             else:
-                print(f"Contact {name} does not exist.")
+                print(f"Contact {contact} does not exist.")
         break
     else:
-        current_name, current_number = info.split("-")
-        phonebook[current_name] = current_number
-        # info_list = info.split("-")
-        # current_name = info_list[0]
-        # current_number = info_list[1]
-        # if current_name not in phonebook:
-        #     phonebook[current_name] = current_number
-        # else:
-        #     phonebook[current_name] = current_number
+        name_and_phone_number = info.split("-")
+        name = name_and_phone_number[0]
+        phone_number = name_and_phone_number[1]
+        if name not in phonebook:
+            phonebook[name] = phone_number
+        else:
+            phonebook[name] = phone_number
+
+
+# phonebook = {}
+# while True:
+#     info = input()
+#     if info.isdigit():
+#         number = int(info)
+#         for information in range(1, number + 1):
+#             name = input()
+#             if name in phonebook:   # if name in phonebook.keys():
+#                 number = phonebook[name]
+#                 print(f"{name} -> {number}")
+#             else:
+#                 print(f"Contact {name} does not exist.")
+#         break
+#     else:
+#         current_name, current_number = info.split("-")
+#         phonebook[current_name] = current_number
+#         # info_list = info.split("-")
+#         # current_name = info_list[0]
+#         # current_number = info_list[1]
+#         # if current_name not in phonebook:
+#         #     phonebook[current_name] = current_number
+#         # else:
+#         #     phonebook[current_name] = current_number
 
 
 # phonebook = {}
