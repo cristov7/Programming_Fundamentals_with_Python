6cc94b58bfdeb99da91500932ac670edfd97cf09
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-11-09 02:55:58 +0200

    Python
    Python

--- (I) - Dictionaries/7.2 - EXE (Dictionaries)/03. Capitals.py
+++ (I) - Dictionaries/7.2 - EXE (Dictionaries)/03. Capitals.py
@@ -1,8 +1,17 @@
-country_list = input().split(", ")
-capitals_list = input().split(", ")
-dictionary = {country_list[index]: capitals_list[index] for index in range(len(country_list))}
-for country, capital in dictionary.items():
+country_names = input().split(", ")
+capital_cities = input().split(", ")
+# len(country_names) == len(capital_cities)
+my_dict = {country_names[index]: capital_cities[index] for index in range(len(country_names))}   # comprehension
+# my_dict = dict(zip(country_names, capital_cities))   # zip
+for country, capital in my_dict.items():
     print(f"{country} -> {capital}")
+
+
+# country_list = input().split(", ")
+# capitals_list = input().split(", ")
+# dictionary = {country_list[index]: capitals_list[index] for index in range(len(country_list))}
+# for country, capital in dictionary.items():
+#     print(f"{country} -> {capital}")
 
 
 # country_list = input().split(", ")
