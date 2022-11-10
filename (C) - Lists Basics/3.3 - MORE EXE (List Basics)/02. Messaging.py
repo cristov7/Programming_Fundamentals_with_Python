52ef43b855c08bd837a7dbf913063f45efeda39b
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-11-10 14:47:54 +0200

    Python
    Python

--- (C) - Lists Basics/3.3 - MORE EXE (List Basics)/02. Messaging.py
+++ (C) - Lists Basics/3.3 - MORE EXE (List Basics)/02. Messaging.py
@@ -24,20 +24,3 @@ for index, value in enumerate(index_per_message):
         current_symbol = message_as_string[value + counter]
         print(current_symbol, end="")
         counter += 1
-
-
-# nums_list = list(map(int, input().split()))
-# string = list(input())
-# idx = []
-# for num in nums_list:
-#     sum_of_digits = 0
-#     str_num = str(num)
-#     for d in str_num:
-#         sum_of_digits += int(d)
-#     idx.append(sum_of_digits)
-# message = []
-# for i in idx:
-#     if i > len(string):
-#         i -= len(string)
-#     message.append(string.pop(i))
-# print(f'{"".join(message)}')
