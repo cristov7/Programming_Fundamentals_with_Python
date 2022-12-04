051458199b1cfdf7c1cfbad9069b653df5459a2d
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-12-04 16:48:00 +0200

    Python
    Python

--- (N) - REGULAR FINAL EXAM/REGULAR FINAL EXAM/02. Boss Rush.py
+++ (N) - REGULAR FINAL EXAM/REGULAR FINAL EXAM/02. Boss Rush.py
@@ -2,16 +2,16 @@ import re
 regex = r"\|([A-Z]{4,})\|\:\#([A-Za-z]+\s{1}[A-Za-z]+)\#"
 count_bosses = int(input())
 for boss in range(1, count_bosses + 1):
-    command = input()
-    valid_list = re.findall(regex, command)
+    info = input()
+    valid_list = re.findall(regex, info)
     if valid_list:
         for boss_info in valid_list:
             boss_name = boss_info[0]
-            title = boss_info[1]
-            strength = len(boss_name)
-            armor = len(title)
-            print(f"{boss_name}, The {title}")
-            print(f">> Strength: {strength}")
-            print(f">> Armor: {armor}")
+            boss_title = boss_info[1]
+            boss_strength = len(boss_name)
+            boss_armor = len(boss_title)
+            print(f"{boss_name}, The {boss_title}")
+            print(f">> Strength: {boss_strength}")
+            print(f">> Armor: {boss_armor}")
     else:
         print("Access denied!")
