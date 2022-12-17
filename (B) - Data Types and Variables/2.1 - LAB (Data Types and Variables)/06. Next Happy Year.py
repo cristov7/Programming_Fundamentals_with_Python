394f6486c48e596527c75e2ceb0fbf20edd8c488
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2022-12-17 19:00:02 +0200

    Python
    Python

--- (B) - Data Types and Variables/2.1 - LAB (Data Types and Variables)/06. Next Happy Year.py
+++ (B) - Data Types and Variables/2.1 - LAB (Data Types and Variables)/06. Next Happy Year.py
@@ -1,12 +1,28 @@
 year = int(input())
-happy_year_condition = False
-while not happy_year_condition:
-    year += 1
-    set_year = set()
-    for i in range(len(str(year))):
-        set_year.add(str(year)[i])
-    happy_year_condition = len(set_year) == len(str(year))
-print(year)
+current_year = year
+while True:
+    current_year += 1
+    current_year_as_string = str(current_year)
+    digits_list = [digit for digit in current_year_as_string]
+    unique_digits_list = list(set(digits_list))
+    length_digits_list = len(digits_list)
+    length_unique_digits_list = len(unique_digits_list)
+    if length_digits_list == length_unique_digits_list:
+        print(current_year)
+        break
+    else:
+        continue
+
+
+# year = int(input())
+# happy_year_condition = False
+# while not happy_year_condition:
+#     year += 1
+#     set_year = set()
+#     for i in range(len(str(year))):
+#         set_year.add(str(year)[i])
+#     happy_year_condition = len(set_year) == len(str(year))
+# print(year)
 
 
 # year = int(input())
