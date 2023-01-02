817418ea2bc0ce557577920ec9e144901893ccbf
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2023-01-02 06:26:01 +0200

    Python
    Python

--- (C) - Lists Basics/3.2 - EXE (Lists Basics)/04. Number Beggars.py
+++ (C) - Lists Basics/3.2 - EXE (Lists Basics)/04. Number Beggars.py
@@ -1,23 +1,38 @@
-numbers_as_string = input()
-count_beggars = int(input())
-numbers_as_string_in_list = numbers_as_string.split(", ")
-numbers_as_integer_in_list = list()
-for element in numbers_as_string_in_list:
-    current_element = int(element)
-    numbers_as_integer_in_list.append(current_element)
-sum_per_beggar_in_list = []
-for beggar in range(count_beggars):
-    index = beggar
-    sum_per_beggar = 0
-    for takes in range(len(numbers_as_integer_in_list)):
-        if index < len(numbers_as_integer_in_list):
-            current_take = numbers_as_integer_in_list[index]
-            sum_per_beggar += current_take
-            index += count_beggars
-        else:
-            break
-    sum_per_beggar_in_list.append(sum_per_beggar)
-print(sum_per_beggar_in_list)
+items_list = list(map(int, input().split(", ")))
+beggars_list = [0 for beggar in range(int(input()))]
+if len(items_list) == len(beggars_list):
+    for index in range(len(items_list)):
+        beggars_list[index] += items_list[index]
+elif len(items_list) < len(beggars_list):
+    for index, item in enumerate(items_list):
+        beggars_list[index] += item
+elif len(items_list) > len(beggars_list):
+    for index_beggar, current_beggar in enumerate(beggars_list):
+        for index_item in range(index_beggar, len(items_list), len(beggars_list)):
+            beggars_list[index_beggar] += items_list[index_item]
+print(beggars_list)
+
+
+# numbers_as_string = input()
+# count_beggars = int(input())
+# numbers_as_string_in_list = numbers_as_string.split(", ")
+# numbers_as_integer_in_list = list()
+# for element in numbers_as_string_in_list:
+#     current_element = int(element)
+#     numbers_as_integer_in_list.append(current_element)
+# sum_per_beggar_in_list = []
+# for beggar in range(count_beggars):
+#     index = beggar
+#     sum_per_beggar = 0
+#     for takes in range(len(numbers_as_integer_in_list)):
+#         if index < len(numbers_as_integer_in_list):
+#             current_take = numbers_as_integer_in_list[index]
+#             sum_per_beggar += current_take
+#             index += count_beggars
+#         else:
+#             break
+#     sum_per_beggar_in_list.append(sum_per_beggar)
+# print(sum_per_beggar_in_list)
 
 
 # takes_as_strings = input()
