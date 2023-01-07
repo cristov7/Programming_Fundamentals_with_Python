afab46b24f8cdfd9d4df78633e09c137b90e293a
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2023-01-07 08:34:51 +0200

    Python
    Python

--- (A) - Basic Syntax, Conditional Statements and Loops/1.3 - MORE EXE (Basic Syntax, Conditional Statements and Loops)/01. Find The Largest.py
+++ (A) - Basic Syntax, Conditional Statements and Loops/1.3 - MORE EXE (Basic Syntax, Conditional Statements and Loops)/01. Find The Largest.py
@@ -1,12 +1,19 @@
 number = input()
-digits_list = []
-for digit in number:
-    current_digit = int(digit)
-    digits_list.append(current_digit)
-digits_list.sort(reverse=True)
-update_digits_list_as_string = []
-for element in digits_list:
-    current_element = str(element)
-    update_digits_list_as_string.append(current_element)
-update_number = ("".join(update_digits_list_as_string))
-print(update_number)
+numbers_list = [num for num in number]
+sorted_numbers_list = sorted(numbers_list)
+sorted_numbers_list.reverse()
+print("".join(sorted_numbers_list))
+
+
+# number = input()
+# digits_list = []
+# for digit in number:
+#     current_digit = int(digit)
+#     digits_list.append(current_digit)
+# digits_list.sort(reverse=True)
+# update_digits_list_as_string = []
+# for element in digits_list:
+#     current_element = str(element)
+#     update_digits_list_as_string.append(current_element)
+# update_number = ("".join(update_digits_list_as_string))
+# print(update_number)
