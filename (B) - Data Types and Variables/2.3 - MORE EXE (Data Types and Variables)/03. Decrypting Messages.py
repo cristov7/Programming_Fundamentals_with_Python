f8c82d30d8c1155b52b2d8befb40b1dda5f0411e
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2023-01-07 08:39:29 +0200

    Python
    Python

--- (B) - Data Types and Variables/2.3 - MORE EXE (Data Types and Variables)/03. Decrypting Messages.py
+++ (B) - Data Types and Variables/2.3 - MORE EXE (Data Types and Variables)/03. Decrypting Messages.py
@@ -1,3 +1,15 @@
+key = int(input())
+message_list = []
+count_rows = int(input())
+for row in range(1, count_rows + 1):
+    letter = input()
+    ord_letter = ord(letter)
+    new_ord_letter = ord_letter + key
+    new_letter = chr(new_ord_letter)
+    message_list.append(new_letter)
+print("".join(message_list))
+
+
 # key = int(input())
 # count_rows = int(input())
 # for rows in range(1, count_rows + 1):
@@ -6,11 +18,11 @@
 #     print(update_letter, end="")
 
 
-key = int(input())
-count_rows = int(input())
-for letters in range(1, count_rows + 1):
-    input_letter = input()
-    letter_as_ord = ord(input_letter)
-    update_letter_as_ord = letter_as_ord + key
-    letter_as_chr = chr(update_letter_as_ord)
-    print(letter_as_chr, end="")
+# key = int(input())
+# count_rows = int(input())
+# for letters in range(1, count_rows + 1):
+#     input_letter = input()
+#     letter_as_ord = ord(input_letter)
+#     update_letter_as_ord = letter_as_ord + key
+#     letter_as_chr = chr(update_letter_as_ord)
+#     print(letter_as_chr, end="")
