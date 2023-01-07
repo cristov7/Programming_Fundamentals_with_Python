ad2f5adcec90e1dca1ce2ac6c2b0cacfe2315439
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2023-01-07 08:36:40 +0200

    Python
    Python

--- (A) - Basic Syntax, Conditional Statements and Loops/1.3 - MORE EXE (Basic Syntax, Conditional Statements and Loops)/03. Wolf In Sheep's Clothing.py
+++ (A) - Basic Syntax, Conditional Statements and Loops/1.3 - MORE EXE (Basic Syntax, Conditional Statements and Loops)/03. Wolf In Sheep's Clothing.py
@@ -1,16 +1,29 @@
-animals_in_list = input().split(", ")
-animals_in_list.reverse()
-if animals_in_list[0] == "wolf":
-    print("Please go away and stop eating my sheep")
-else:
-    sheep_in_danger = 0
-    for index, value in enumerate(animals_in_list):
-        if value == "wolf":
-            print(f"Oi! Sheep number {sheep_in_danger}! You are about to be eaten by a wolf!")
-            break
-        else:
-            sheep_in_danger += 1
-            continue
+sheep_list = input().split(", ")
+sheep_list.append("me")
+sheep_list.reverse()
+for index, sheep in enumerate(sheep_list):
+    if index == 1 and sheep == "wolf":
+        print("Please go away and stop eating my sheep")
+        break
+    elif sheep == "wolf":
+        print(f"Oi! Sheep number {index - 1}! You are about to be eaten by a wolf!")
+        break
+    else:
+        continue
+
+
+# animals_in_list = input().split(", ")
+# animals_in_list.reverse()
+# if animals_in_list[0] == "wolf":
+#     print("Please go away and stop eating my sheep")
+# else:
+#     sheep_in_danger = 0
+#     for index, value in enumerate(animals_in_list):
+#         if value == "wolf":
+#             print(f"Oi! Sheep number {sheep_in_danger}! You are about to be eaten by a wolf!")
+#             break
+#         else:
+#             sheep_in_danger += 1
 
 
 # animals_in_list = input().split(", ")
