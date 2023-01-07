a3dc73425f09031357c6af692149ae1d3725b282
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2023-01-07 08:33:14 +0200

    Python
    Python

--- (C) - Lists Basics/3.3 - MORE EXE (List Basics)/06. List Manipulator.py
+++ (C) - Lists Basics/3.3 - MORE EXE (List Basics)/06. List Manipulator.py
@@ -40,3 +40,170 @@ while command[0] != "end":
             print(f"Invalid count")
     command = input().split()
 print(numbers)
+
+
+# numbers_List = list(map(int, input().split()))
+# while True:
+#     command = input()
+#     if command == "end":
+#         break
+#     else:
+#         command_list = command.split()
+#         info = command_list[0]
+#         if info == "exchange":
+#             index = int(command_list[1])
+#             if 0 <= index < len(numbers_List):
+#                 end_part_list = numbers_List[:index + 1]
+#                 first_part_list = numbers_List[index + 1:]
+#                 numbers_List = first_part_list + end_part_list
+#             else:
+#                 print("Invalid index")
+#         elif info == "max":
+#             more_info = command_list[1]
+#             if more_info == "even":
+#                 if len(numbers_List) > 1:
+#                     max_number = ""
+#                     max_index = ""
+#                     for index, number in enumerate(numbers_List):
+#                         if number % 2 == 0:
+#                             if max_number == "":
+#                                 max_number = number
+#                                 max_index = index
+#                             elif number >= max_number:
+#                                 max_number = number
+#                                 max_index = index
+#                         else:
+#                             continue
+#                     if max_index == "":
+#                         print("No matches")
+#                     else:
+#                         print(max_index)
+#                 else:
+#                     print("No matches")
+#             elif more_info == "odd":
+#                 if len(numbers_List) > 0:
+#                     max_number = ""
+#                     max_index = ""
+#                     for index, number in enumerate(numbers_List):
+#                         if number % 2:
+#                             if max_number == "":
+#                                 max_number = number
+#                                 max_index = index
+#                             elif number >= max_number:
+#                                 max_number = number
+#                                 max_index = index
+#                         else:
+#                             continue
+#                     if max_index == "":
+#                         print("No matches")
+#                     else:
+#                         print(max_index)
+#                 else:
+#                     print("No matches")
+#         elif info == "min":
+#             more_info = command_list[1]
+#             if more_info == "even":
+#                 if len(numbers_List) > 1:
+#                     min_number = ""
+#                     max_index = ""
+#                     for index, number in enumerate(numbers_List):
+#                         if number % 2 == 0:
+#                             if min_number == "":
+#                                 min_number = number
+#                                 max_index = index
+#                             elif number <= min_number:
+#                                 min_number = number
+#                                 max_index = index
+#                         else:
+#                             continue
+#                     if max_index == "":
+#                         print("No matches")
+#                     else:
+#                         print(max_index)
+#                 else:
+#                     print("No matches")
+#             elif more_info == "odd":
+#                 if len(numbers_List) > 0:
+#                     min_number = ""
+#                     max_index = ""
+#                     for index, number in enumerate(numbers_List):
+#                         if number % 2:
+#                             if min_number == "":
+#                                 min_number = number
+#                                 max_index = index
+#                             elif number <= min_number:
+#                                 min_number = number
+#                                 max_index = index
+#                         else:
+#                             continue
+#                     if max_index == "":
+#                         print("No matches")
+#                     else:
+#                         print(max_index)
+#                 else:
+#                     print("No matches")
+
+#         elif info == "first":
+#             count = int(command_list[1])
+#             more_info = command_list[2]
+#             if more_info == "even":
+#                 if count > len(numbers_List):
+#                     print("Invalid count")
+#                 else:
+#                     even_list = [number for number in numbers_List if number % 2 == 0]
+#                     if len(even_list) >= count:
+#                         first_count_even_list = []
+#                         for number in even_list:
+#                             first_count_even_list.append(number)
+#                             if len(first_count_even_list) == count:
+#                                 break
+#                             else:
+#                                 continue
+#                         print(first_count_even_list)
+#                     else:
+#                         print(even_list)
+
+#             elif more_info == "odd":
+#                 if count > len(numbers_List):
+#                     print("Invalid count")
+#                 else:
+#                     odd_list = [number for number in numbers_List if number % 2]
+#                     if len(odd_list) >= count:
+#                         first_count_odd_list = []
+#                         for number in odd_list:
+#                             first_count_odd_list.append(number)
+#                             if len(first_count_odd_list) == count:
+#                                 break
+#                             else:
+#                                 continue
+#                         print(first_count_odd_list)
+#                     else:
+#                         print(odd_list)
+
+#         elif info == "last":
+#             count = int(command_list[1])
+#             more_info = command_list[2]
+#             if more_info == "even":
+#                 if count > len(numbers_List):
+#                     print("Invalid count")
+#                 else:
+#                     even_list = [number for number in numbers_List if number % 2 == 0]
+#                     if len(even_list) == 0:
+#                         print("[]")
+#                     else:
+#                         start_index = count * (-1)
+#                         last_count_even_list = even_list[start_index:]
+#                         print(last_count_even_list)
+
+#             elif more_info == "odd":
+#                 if count > len(numbers_List):
+#                     print("Invalid count")
+#                 else:
+#                     odd_list = [number for number in numbers_List if number % 2]
+#                     if len(odd_list) == 0:
+#                         print("[]")
+#                     else:
+#                         start_index = count * (-1)
+#                         last_count_odd_list = odd_list[start_index:]
+#                         print(last_count_odd_list)
+# print(numbers_List)
