9f267639d290ac5f9da4082f6fd5670d8c2f7b6d
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2023-01-07 07:44:08 +0200

    Python
    Python

--- (C) - Lists Basics/3.3 - MORE EXE (List Basics)/01. Zeros to Back.py
+++ (C) - Lists Basics/3.3 - MORE EXE (List Basics)/01. Zeros to Back.py
@@ -1,17 +1,26 @@
-numbers_as_string = input()
-numbers_as_string_in_list = numbers_as_string.split(", ")
-numbers_as_integers_in_list = list()
-for element in numbers_as_string_in_list:
-    current_element = int(element)
-    numbers_as_integers_in_list.append(current_element)
-count_zero = numbers_as_integers_in_list.count(0)
-while 0 in numbers_as_integers_in_list:
-    numbers_as_integers_in_list.remove(0)
-counter = 0
-while counter < count_zero:
-    counter += 1
-    numbers_as_integers_in_list.append(0)
-print(numbers_as_integers_in_list)
+single_string_list = list(map(int, input().split(", ")))
+count_zeros = single_string_list.count(0)
+while 0 in single_string_list:
+    single_string_list.remove(0)
+for zero in range(1, count_zeros + 1):
+    single_string_list.append(0)
+print(single_string_list)
+
+
+# numbers_as_string = input()
+# numbers_as_string_in_list = numbers_as_string.split(", ")
+# numbers_as_integers_in_list = list()
+# for element in numbers_as_string_in_list:
+#     current_element = int(element)
+#     numbers_as_integers_in_list.append(current_element)
+# count_zero = numbers_as_integers_in_list.count(0)
+# while 0 in numbers_as_integers_in_list:
+#     numbers_as_integers_in_list.remove(0)
+# counter = 0
+# while counter < count_zero:
+#     counter += 1
+#     numbers_as_integers_in_list.append(0)
+# print(numbers_as_integers_in_list)
 
 
 # num_list = [int(num) for num in input().split(', ')]
