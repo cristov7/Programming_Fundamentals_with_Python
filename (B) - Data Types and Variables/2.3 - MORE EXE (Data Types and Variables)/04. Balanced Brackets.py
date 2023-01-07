37efb524221bcf679980cd1ac8ba3c739761195a
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2023-01-07 08:49:52 +0200

    Python
    Python

--- (B) - Data Types and Variables/2.3 - MORE EXE (Data Types and Variables)/04. Balanced Brackets.py
+++ (B) - Data Types and Variables/2.3 - MORE EXE (Data Types and Variables)/04. Balanced Brackets.py
@@ -1,45 +1,47 @@
 number_of_lines = int(input())
-is_open = False
-is_close = False
-counter = 0
-for line in range(number_of_lines):
-    text = input()
-    if "(" and ")" not in text:
-        counter += 1
-    for char in text:
-        if char == "(":
-            if not is_open and not is_close:
-                is_open = True
-            else:
-                is_open = False
-        elif char == ")":
-            is_close = True
-        if is_open and is_close:
-            is_open = False
-            is_close = False
-if not is_close and not is_open and number_of_lines != counter:
+calculating = ""
+info = True
+for line in range(1, number_of_lines + 1):
+    random_string = input()
+    if random_string == "(" and calculating == "":
+        calculating += random_string
+    elif random_string == "(" and calculating != "":
+        info = False
+        break
+    elif random_string == ")" and calculating == "(":
+        calculating = ""
+    elif random_string == ")" and calculating == "":
+        info = False
+        break
+    else:
+        continue
+if info:
     print("BALANCED")
 else:
     print("UNBALANCED")
 
 
-# balanced_or_unbalanced = ""
-# symbol_list = []
-# number = int(input())
-# for rows in range(1, number + 1):
-#     symbol = input()
-#     if symbol == "(":
-#         symbol_list.append(symbol)
-#         balanced_or_unbalanced = "UNBALANCED"
-#     elif symbol == ")":
-#         symbol_list.append(symbol)
-#         if "(" in symbol_list:
-#             symbol_list.remove("(")
-#             symbol_list.remove(")")
-#             balanced_or_unbalanced = "BALANCED"
-#         else:
-#             balanced_or_unbalanced = "UNBALANCED"
-#             break
-#     else:
-#         continue
-# print(balanced_or_unbalanced)
+# number_of_lines = int(input())
+# is_open = False
+# is_close = False
+# counter = 0
+# for line in range(number_of_lines):
+#     text = input()
+#     if "(" and ")" not in text:
+#         counter += 1
+#     for char in text:
+#         if char == "(":
+#             if not is_open and not is_close:
+#                 is_open = True
+#             else:
+#                 is_open = False
+#         elif char == ")":
+#             is_close = True
+#         if is_open and is_close:
+#             is_open = False
+#             is_close = False
+# if not is_close and not is_open and number_of_lines != counter:
+#     print("BALANCED")
+# else:
+#     print("UNBALANCED")
+
