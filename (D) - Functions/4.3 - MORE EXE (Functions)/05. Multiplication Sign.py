855262e52574eb4057037b65a7b9ff5f35b48a10
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2023-01-08 10:07:29 +0200

    Python
    Python

--- (D) - Functions/4.3 - MORE EXE (Functions)/05. Multiplication Sign.py
+++ (D) - Functions/4.3 - MORE EXE (Functions)/05. Multiplication Sign.py
@@ -1,40 +1,42 @@
-def multiplication_sign(number_1: int, number_2: int, number_3: int):
-    numbers_list = [number_1, number_2, number_3]
-    counter_minus = 0
-    for number in numbers_list:
-        if number == 0:
-            return "zero"
+def multiplication_function(some_list: list):
+    result = 0
+    for index, number in enumerate(some_list):
+        if index == 0:
+            result = number
         else:
-            if number < 0:
-                counter_minus += 1
-            else:
-                continue
-    if counter_minus % 2 == 0:
+            multiply = result * number
+            result = multiply
+    if result > 0:
         return "positive"
-    else:
+    elif result < 0:
         return "negative"
+    else:
+        return "zero"
 
 
-first_number = int(input())
-second_number = int(input())
-third_number = int(input())
-print(multiplication_sign(first_number, second_number, third_number))
+numbers_list = [int(input()) for number in range(3)]
+calculation = multiplication_function(numbers_list)
+print(calculation)
 
 
-# def chk_sign(l):
+# def multiplication_sign(number_1: int, number_2: int, number_3: int):
+#     numbers_list = [number_1, number_2, number_3]
+#     counter_minus = 0
+#     for number in numbers_list:
+#         if number == 0:
+#             return "zero"
+#         else:
+#             if number < 0:
+#                 counter_minus += 1
+#             else:
+#                 continue
+#     if counter_minus % 2 == 0:
+#         return "positive"
+#     else:
+#         return "negative"
 #
-#     if all(l):
 #
-#         negatives = [num for num in l if abs(num) != num]
-#
-#         if len(negatives) % 2 == 1:
-#             return 'negative'
-#
-#         return 'positive'
-#
-#     return 'zero'
-#
-#
-# nums = [int(input()) for _ in range(3)]
-#
-# print(chk_sign(nums))
+# first_number = int(input())
+# second_number = int(input())
+# third_number = int(input())
+# print(multiplication_sign(first_number, second_number, third_number))
